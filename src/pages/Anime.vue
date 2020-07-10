83bf87194f3b8ae1d6e952bd3d89dbd780406fe8
Jabster28 <justynboyer@gmail.com>
2020-07-10 22:22:01 +0000

    fix: fixed janky display on bigger screens

--- src/pages/Anime.vue
+++ src/pages/Anime.vue
@@ -1,38 +1,53 @@
 <template>
-  <q-page class="row items-center justify-evenly">
+  <q-page class="items-center justify-evenly">
     <div class="post">
       <div v-if="error" class="error">
         {{ error }}
       </div>
 
       <div v-if="anime.mal_id" class="content">
-        <div class="col-8 q-mx-lg items-center justify-evenly">
-          <q-img :src="anime.image_url" />
-          <q-btn
-            class="q-ma-lg"
-            color="secondary"
-            clickable
-            type="a"
-            target="_blank"
-            :href="anime.url"
-          >
-            Open in MAL</q-btn
-          >
-          <q-btn
-            v-if="anime.trailer_url"
-            target="_blank"
-            flat
-            clickable
-            type="a"
-            :href="anime.trailer_url"
-            >Trailer</q-btn
-          >
-          <h2>{{ anime.title }}</h2>
-          <h6 class="disabled">{{ anime.title_english }}</h6>
-          <h5><q-icon name="stars" /> {{ anime.score * 10 }}%</h5>
-          <h5><q-icon name="visibility" /> {{ norm(anime.members) }}</h5>
-          <h5><q-icon name="star" />{{ norm(anime.favorites) }}</h5>
-          <h5>Ranked: #{{ anime.rank }}</h5>
+        <div
+          :class="[
+            'q-mx-lg',
+            'items-center',
+            'justify-evenly',
+            $q.screen.width > 800 ? 'row' : 'col'
+          ]"
+        >
+          <div class="col-5">
+            <q-img :src="anime.image_url" />
+            <q-btn
+              class="q-ma-lg"
+              color="secondary"
+              clickable
+              type="a"
+              target="_blank"
+              :href="anime.url"
+            >
+              Open in MAL</q-btn
+            >
+            <q-btn
+              v-if="anime.trailer_url"
+              target="_blank"
+              flat
+              clickable
+              type="a"
+              :href="anime.trailer_url"
+              >Trailer</q-btn
+            >
+          </div>
+          <div class="col-5">
+            <h2>{{ anime.title }}</h2>
+
+            <h6 class="disabled">{{ anime.title_english }}</h6>
+            <p class="text-justify">{{ anime.synopsis }}</p>
+            <h5>
+              <q-icon name="stars" /> {{ anime.score.toPrecision(3) }} / 10
+            </h5>
+            <h5><q-icon name="visibility" /> {{ norm(anime.members) }}</h5>
+            <h5><q-icon name="star" /> {{ norm(anime.favorites) }}</h5>
+            <h5 v-if="anime.rank"># {{ norm(anime.rank) }}</h5>
+          </div>
         </div>
       </div>
     </div>
