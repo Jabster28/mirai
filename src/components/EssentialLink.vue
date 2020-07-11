8b362e9892120349d57ef636cf49b5534fd2a3e7
Jabster28 <justynboyer@gmail.com>
2020-07-11 10:28:14 +0000

    chore: add support for external links

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,16 +1,30 @@
 <template>
-  <q-item clickable tag="a" :to="link">
-    <q-item-section v-if="icon" avatar>
-      <q-icon :name="icon" />
-    </q-item-section>
+  <div>
+    <q-item v-if="external" clickable tag="a" target="_blank" :href="link">
+      <q-item-section v-if="icon" avatar>
+        <q-icon :name="icon" />
+      </q-item-section>
 
-    <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>
-        {{ caption }}
-      </q-item-label>
-    </q-item-section>
-  </q-item>
+      <q-item-section>
+        <q-item-label>{{ title }}</q-item-label>
+        <q-item-label caption>
+          {{ caption }}
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item v-else clickable type="a" :to="link">
+      <q-item-section v-if="icon" avatar>
+        <q-icon :name="icon" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{ title }}</q-item-label>
+        <q-item-label caption>
+          {{ caption }}
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+  </div>
 </template>
 
 <script>
@@ -31,7 +45,10 @@ export default {
       type: String,
       default: '/'
     },
-
+    external: {
+      type: Boolean,
+      default: false
+    },
     icon: {
       type: String,
       default: ''
