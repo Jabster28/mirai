6030c3fbeca6599347f272bd061647e75cab643c
Jabster28 <justynboyer@gmail.com>
2021-04-07 03:01:34 +0100

    feat(/): add OnHold section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -39,6 +39,25 @@
         </div>
       </q-scroll-area>
     </div>
+    <div v-if="hold.length" class="col-10">
+      <h4>Animes you've paused:</h4>
+      <q-scroll-area
+        horizontal
+        :visible="false"
+        class="q-ma-md"
+        style="height: 400px;"
+      >
+        <div class="row no-wrap">
+          <AnimeCard
+            :trunc="30"
+            v-for="anime in hold"
+            :key="anime.mal_id"
+            :anime="anime"
+            home
+          />
+        </div>
+      </q-scroll-area>
+    </div>
   </q-page>
 </template>
 
@@ -70,12 +89,23 @@ export default Vue.extend({
           if (Array.isArray(data.data.data)) this.suggestions = data.data.data;
         })
         .catch(e => console.log(e));
+      axios
+        .get(
+          `https://api.jikan.moe/v3/user/${
+            this.$q.cookies.get('mal_auth').name
+          }/animelist/OnHold`
+        )
+        .then(data => {
+          if (Array.isArray(data.data.anime)) this.hold = data.data.anime;
+        })
+        .catch(e => console.log(e));
     }
   },
   data() {
     return {
       top: null,
-      suggestions: []
+      suggestions: [],
+      hold: []
     };
   }
 });
